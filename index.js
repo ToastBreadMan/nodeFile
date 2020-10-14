0ea495cd2e31eaf45f35dbb3464e6d41038806fa
Toast <52469686+ToastBreadMan@users.noreply.github.com>
2020-10-14 17:25:16 +0000

    newer version

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(upload())
 
 var requestName
 var filename
+var requestStatus = false
 
 app.listen(3000, () => console.log("server on port 3000"))
 
@@ -15,17 +16,21 @@ app.get("/", (req, res) => {
 })
 
 app.post("/name", (req,res) => {
-    filename = `/downloads/${req.body.input}.jpg`
-    fs.exists(filename, () => {
-        requestName = req.body.input
+    filename = `./downloads/${req.body.input}.jpg`
+    console.log(filename)
+    console.log()
+    fs.exists(filename, (exists) => {
+        requestStatus = exists
+       // console.log("file:"+requestName, exists ? " exists" : "not existing")
     })
 })
 
 app.get("/download", (req, res) => {
     console.log(requestName)
-        if(!requestName){
+        if(!requestStatus){
             res.send("404 du affe")
             console.log("404")
+            requestName = ""
         }
         else{
             res.download(__dirname + `/downloads/${requestName}.jpg`, `${requestName}`)
@@ -44,6 +49,15 @@ app.post("/upload", (req,res) => {
         })
         console.log(filename)
     }
-   // res.send("success hope you get redirected"
-   res.redirect("/")
+   // res.send("success hope you get redirected)"
+   res.redirect("/",301)
+})
+
+app.get("/all", (req,res) => {
+    fs.readdir(__dirname + "/downloads/", (err,files) => {
+        var data = {
+            fileData : files 
+        }
+        res.json(data)
+    })
 })
